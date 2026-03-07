3ac571210715a5c453671472d81c6b049717943d
standlx <lantianye@163.com>
2026-03-08 00:22:45 +0800

    refactor: update fetchers __init__.py with all exports

--- finscraper/fetchers/__init__.py
+++ finscraper/fetchers/__init__.py
@@ -1,1 +1,17 @@
 """Fetchers module for FinScraper."""
+from finscraper.fetchers.index import IndexFetcher
+from finscraper.fetchers.north_flow import NorthFlowFetcher
+from finscraper.fetchers.sector import SectorFetcher
+from finscraper.fetchers.commodity import CommodityFetcher
+from finscraper.fetchers.money_flow import MoneyFlowFetcher
+from finscraper.fetchers.news import NewsFetcher
+
+__all__ = [
+    "IndexFetcher",
+    "NorthFlowFetcher",
+    "SectorFetcher",
+    "CommodityFetcher",
+    "MoneyFlowFetcher",
+    "NewsFetcher",
+]
+
